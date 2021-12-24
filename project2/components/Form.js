c3cd5d14c2e940774edebc55ccee058e00905207
jjanmo <jjanmo07@hanmail.net>
2021-12-24 20:15:07 +0900

    feat: add form animation using css transform

--- project2/components/Form.js
+++ project2/components/Form.js
@@ -1,21 +1,15 @@
-const Form = () => {
-  const $wrapper = document.querySelector(".wrapper");
-  $wrapper.innerHTML = `
-      <div class="form-container hidden">
-        <div class="close-button">❌</div>
-        <h2>Add your POST 📝</h2>
-        <form>
-          <div class="row">
-            <label for="title">TITLE</label>
-            <input type="text" name="title" id="title"/>
-            <input type=hidden name="id" id="id"/>
-          </div>
-          <div class="row">
-            <label for="description">DESCRIPTION</label>
-            <textarea name="description" id="description" rows="5" cols="33"></textarea>
-          </div>
-          <button type="submit" class="submit-button" >SUBMIT</button>
-        </form>
-      </div>
-    `;
-};
+const $formContainer = document.querySelector('.form-container');
+const $addButton = document.querySelector('.add-button');
+
+$addButton.addEventListener('click', (e) => {
+  const target = e.target;
+  if ($formContainer.className.includes('hidden')) {
+    $formContainer.classList.remove('hidden');
+    target.classList.add('clicked');
+  } else {
+    $formContainer.classList.add('hidden');
+    target.classList.remove('clicked');
+  }
+});
+
+function render() {}
